a8a01fa7e8e15f6a13e96ba7974e7b341e4dbab9
rbachala <ramachandra.bachala@intel.com>
2018-06-15 15:28:40 -0700

    added new models for user info. Added tests to save a user record using mongoose.

---
+++ server/controllers/usermanagement/login.db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'saveUserInfo' : function (user, saltedhash, email, firstname, lastname) {
+        
+    }
+}
--- server/controllers/usermanagement/tests/login.db.js
+++ server/controllers/usermanagement/tests/login.db.js
@@ -2,13 +2,45 @@ const chai = require("chai");
 const expect = chai.expect;
 
 var dblogin = require('../login.db');
+var UserModel = require('../models/usermodel');
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/conman';
+var db;
 
 describe('login db tests', () => {
-    it('should take username and password and return a token', () => {
-        var username = "";
-        var password = "";
-        var userid = dblogin.getuserid(username);
-        var salt = hashit(password + userid);
-        dblogin.login(username, password);
+    before(() => {
+        mongoose.connect(mongoDB);
+        // Get Mongoose to use the global promise library
+        mongoose.Promise = global.Promise;
+        //Get the default connection
+        db = mongoose.connection;
+        //Bind connection to error event (to get notification of connection errors)
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        console.log('database created...', db)
+    });
+    // it('should take username and password and return a token', () => {
+    //     var username = "";
+    //     var password = "";
+    //     var userid = dblogin.getuserid(username);
+    //     var salt = hashit(password, userid);
+    //     dblogin.login(username, password);
+    // });
+
+    it('should take user info and save it in backend data stor', (done) => {
+
+        usermodel = new UserModel({
+            username: "ramu",
+            passwordhash: "somehash"
+        })
+
+        usermodel.save(function (err) {
+            if (err) console.log('error while saving ' + err);
+
+            console.log("success")
+            done();
+        });
     });
 });
